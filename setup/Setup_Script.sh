89955669e776616cfc86a4dce2b850b4ad1f4206
Adam Patni <adampatni27@gmail.com>
2017-08-05 23:20:38 -0500

    Removed spaces from bash file names

--- setup/Setup_Script.sh
+++ setup/Setup_Script.sh
@@ -2,8 +2,8 @@
 
 sudo apt-get update
 sudo apt-get upgrade
-sh enable camera.sh
-sh pip installs.sh
+sh enable_camera.sh
+sh pip_installs.sh
 sh install-opencv3.2.sh
 sh createSystemdFile.sh
 sudo apt-get reboot

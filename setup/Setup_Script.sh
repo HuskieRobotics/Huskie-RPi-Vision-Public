e11850aca7dfe52456db418d8ab40a5350cf8900
Adam Patni <adampatni27@gmail.com>
2017-08-29 15:18:57 -0500

    Testing scripts, need to do at home

--- setup/Setup_Script.sh
+++ setup/Setup_Script.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-sudo apt-get update
-sudo apt-get upgrade
-sh enable_camera.sh
-sh pip_installs.sh
-sh install-opencv3.2.sh
-sh createSystemdFile.sh
+#sudo apt-get update
+
+#sudo apt-get upgrade
+3read -rsp $'Press any key to continue...\n' -n1 key
+
+sudo sh enableCamAdam.sh
+#read -rsp $'Press any key to continue...\n' -n1 key
+
+sudo sh pip_installs.sh
+#read -rsp $'Press any key to continue...\n' -n1 key
+
+sudo sh install-opencv3.2.sh
+
+sudo sh createSystemdFile.sh
+
 sudo apt-get reboot
 
--- setup/enableCamAdam.sh
+++ setup/enableCamAdam.sh
@@ -5,5 +5,3 @@ sudo sed -i 's/start_x=0/start_x=1/g' /boot/config.txt
 
 sudo sed -i 's/gpu_mem=128/gpu_mem=256/g' /boot/config.txt
 
-sudo reboot
-

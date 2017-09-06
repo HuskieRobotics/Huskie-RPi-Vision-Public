8dee170cc0bfc48abb120b3784bd54f7307eb4fd
Adam Patni <adampatni27@gmail.com>
2017-09-06 14:47:38 -0500

    Update Setup_Script.sh

--- setup/Setup_Script.sh
+++ setup/Setup_Script.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
 sudo apt-get -y update
+
 sudo apt-get -y upgrade
 
+sudo sh /home/pi/Huskie-Vision/setup/enableCamAdam.sh
 
-sudo sh /home/pi/Huskie-RPi-Vision-Public/setup/enableCamAdam.sh
+sudo sh /home/pi/Huskie-Vision/setup/pip_installs.sh
 
+sudo sh /home/pi/Huskie-Vision/setup/install-opencv3.2.sh
+cd /home/pi/Huskie-Vision/setup/
 
-sudo sh /home/pi/Huskie-RPi-Vision-Public/setup/pip_installs.sh
-
-
-sudo sh /home/pi/Huskie-RPi-Vision-Public/setup/install-opencv3.2.sh
-cd /home/pi/Huskie-RPi-Vision-Public/setup/
-
-sudo sh /home/pi/Huskie-RPi-Vision-Public/setup/createSystemdFile.sh
+sudo sh /home/pi/Huskie-Vision/setup/createSystemdFile.sh
 
 sudo shutdown -r now

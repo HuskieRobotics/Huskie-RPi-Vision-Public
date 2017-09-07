b021c7df9a244652418a880f4b3327ec5d19ed89
Adam Patni <adampatni27@gmail.com>
2017-09-07 14:52:54 -0500

    Update createSystemdFile.sh

--- setup/createSystemdFile.sh
+++ setup/createSystemdFile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo cp /home/pi/Huskie-RPi-Vision-Public/setup/VisionStartup.service  /etc/systemd/system/
+sudo cp /home/pi/Huskie-Vision/setup/VisionStartup.service  /etc/systemd/system/
 sudo chmod 777 /etc/systemd/system/VisionStartup.service
 sudo systemctl daemon-reload

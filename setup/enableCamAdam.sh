155f29711f4a72509b1bc25ddc4d7307f8773a13
Adam Pi <adampatni27@gmail.com>
2017-09-09 00:10:17 -0500

    Added functionality to enable camera by allowing it to add lines to the config.txt in case they didn't exist

--- setup/enableCamAdam.sh
+++ setup/enableCamAdam.sh
@@ -5,3 +5,8 @@ sudo sed -i 's/start_x=0/start_x=1/g' /boot/config.txt
 
 sudo sed -i 's/gpu_mem=128/gpu_mem=256/g' /boot/config.txt
 
+sudo grep -q 'start_x=1' /boot/config.txt || sudo echo 'start_x=1' >> /boot/config.txt
+
+sudo grep -q 'gpu_mem=256' /boot/config.txt || sudo echo 'gpu_mem=256' >> /boot/config.txt
+
+

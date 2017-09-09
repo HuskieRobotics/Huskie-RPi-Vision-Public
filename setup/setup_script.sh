ba125c46f5abf8ef3a47d5dbfeb77d2594476158
Adam Pi <adampatni27@gmail.com>
2017-09-09 00:14:40 -0500

    renamed a bunch of files

--- setup/setup_script.sh
+++ setup/setup_script.sh
@@ -7,13 +7,13 @@ sudo apt-get -y update
 
 sudo apt-get -y upgrade
 
-sudo sh /home/pi/Huskie-Vision/setup/enableCamAdam.sh
+sudo sh /home/pi/Huskie-Vision/setup/enable_cam.sh
 
 sudo sh /home/pi/Huskie-Vision/setup/pip_installs.sh
 
-sudo sh /home/pi/Huskie-Vision/setup/install-opencv3.2.sh
+sudo sh /home/pi/Huskie-Vision/setup/install_opencv.sh
 cd /home/pi/Huskie-Vision/setup/
 
-sudo sh /home/pi/Huskie-Vision/setup/createSystemdFile.sh
+sudo sh /home/pi/Huskie-Vision/setup/enable_systemd_service.sh
 
 sudo shutdown -r now

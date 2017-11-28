fc842adf9304aa61848a69b0c8141fc1661c4a8a
Adam Patni <adampatni27@gmail.com>
2017-11-27 20:02:56 -0600

    Implemented the Machine Learning into vision, need to test extensively

--- vision-processing/Vision_Main.py
+++ vision-processing/Vision_Main.py
@@ -11,11 +11,11 @@ from picamera import PiCamera
 #User created modules
 import UDP_Client
 from Image_Processing import process_image
-
+import MLPredictor
 
 
 def getVideo():
-    
+
     #Initialize Camera Stream
     camera = PiCamera()
     camera.resolution=(320,240)
@@ -23,13 +23,13 @@ def getVideo():
     #These values are subject to change, use the Testing Suite to determine what range of values you want
     camera.brightness =50
     camera.ISO = 100
-    
+
     camera.shutter_speed = 1000
     rawCapture = PiRGBArray(camera,size=(320,240))
 
     ###Edit the line below and change the IP address to your robot's ip (i.e. "10.30.61.17"), port is an arbitrary number
     client = UDP_Client.Client("Robot IP",9000) #(IP,PORT)
-    
+    predictor = MLPredictor.Predictor('neuralNetGL3')
 
     #frame_time is a pretty precise way of getting the timestamp of your image if you need it
     frame_time = time.time()
@@ -38,13 +38,14 @@ def getVideo():
 
         ###DO YOUR PROCESSING HERE USING OpenCV and the image variable
         ###Refer to the Image Processing module and call its function process_image here
-        process_image(image)
-        
-        
+        targetX, targetY, targetW, targetH = process_image(image)
+        coord3D = predictor.predict3D([targetX,targetY,targetW,targetH])
+        angle = predictor.getAngle(coord3D)
+
         ###Input your data and tags into the list below to send data to the rio
         ###This data is converted to a json string FYI, makes the sending faster
-        client.sendData({"X":0, "Y":0, "Z":0,"Time":frame_time})
-        
+        client.sendData({"Angle":angle,"Time":frame_time})
+
         #this trunctates the stream of images to grab the current image
         rawCapture.truncate(0)
         frame_time = time.time()
